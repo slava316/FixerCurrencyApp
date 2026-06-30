6eae3b2ab211bdd66623b92db5702cd082977b40
slava316 <slava.fomin.9191@gmail.com>
2026-06-30 03:55:03 +0300

    Core: Add JSON models for Fixer API response

---
+++ FixerCurrencyApp/Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FixerCurrencyApp
+{
+    public class FixerResponse
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public long Timestamp { get; set; }
+
+        [JsonPropertyName("base")]
+        public string Base { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("rates")]
+        public Dictionary<string, double> Rates { get; set; }
+
+        [JsonPropertyName("error")]
+        public FixerError Error { get; set; }
+    }
+
+    public class FixerError
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("info")]
+        public string Info { get; set; }
+    }
+}
